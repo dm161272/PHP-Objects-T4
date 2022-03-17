ed90ff73db7bb49bc6ded537eeaf2325c73622d0
dm161272 <58472537+dm161272@users.noreply.github.com>
2022-03-17 12:42:51 +0100

    Add files via upload

---
+++ n1ex1t4PHP/ex1.php
@@ -0,0 +1,19 @@
+
+
+<?php
+
+
+/* 
+
+Creates an Employee class defines its name and salary as attributes.
+Set a initialize method that receives the name and you are as parameters.
+Plant a second print method that prints the name
+and a message if you have to pay taxes or not (if you pay more than 6,000 pay taxes).
+
+
+Create a class *Employee defines his name and salary as attributes.
+Define a method *initialize that receives the name and salary as parameters.
+Propose a second method *print that prints the name and a message if he has 
+or has not paid taxes (if the salary exceeds 6000 he pays taxes).
+
+*/
---
+++ n1ex2t4PHP/ex2.php
@@ -0,0 +1,22 @@
+
+
+<?php
+
+/*
+
+Escribe un programa que defina una clase *Shape con un constructor que reciba 
+como parámetros el ancho y alto. 
+Define dos subclases; Triángulo y Rectángulo que hereden de 
+*Shape y que calculen respectivamente el área de la forma *area().
+
+Al archivo *main define dos objetos, un triángulo y un rectángulo y truca al método *area de cada uno.
+
+
+Write a program that defines a *Shape class with a constructor that takes width and height as parameters.
+Define two subclasses; Triangle and Rectangle that inherit from *Shape and respectively calculate
+the area of the shape *area().
+
+To the file *main define two objects, a triangle and a rectangle and trick the *area method of each one.
+
+
+*/
