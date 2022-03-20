8b5c44222c322d7fefed22f90d10aac9ad6a062f
dm161272 <58472537+dm161272@users.noreply.github.com>
2022-03-20 18:08:15 +0100

    Update ex2.php

--- n1ex2t4PHP/ex2.php
+++ n1ex2t4PHP/ex2.php
@@ -1,22 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      label {
+        display: inline-block;
+        width: 100px;
+        text-align: left;
+      }
+    </style>
+    <title>Document</title>
+</head>
+<body>
 
 
-<?php
-
-/*
-
-Escribe un programa que defina una clase *Shape con un constructor que reciba 
-como parámetros el ancho y alto. 
-Define dos subclases; Triángulo y Rectángulo que hereden de 
-*Shape y que calculen respectivamente el área de la forma *area().
-
-Al archivo *main define dos objetos, un triángulo y un rectángulo y truca al método *area de cada uno.
-
+<!--
 
 Write a program that defines a *Shape class with a constructor that takes width and height as parameters.
-Define two subclasses; Triangle and Rectangle that inherit from *Shape and respectively calculate
-the area of the shape *area().
+ Define two subclasses; Triangle and Rectangle that inherit from 
+ *Shape and respectively calculate the area of the shape *area().
 
 To the file *main define two objects, a triangle and a rectangle and trick the *area method of each one.
 
+-->
 
-*/
+
+
+   
+<h2>Area calculator</h2>
+    
+    <h3>Enter data below:</h3>
+    <form action ="" method ="POST" style="display: table;">
+        <label>Choose shape:</label><br />
+        <label for="rect">Rectangle</label>
+        <input type="radio" id="rect" name="shape" value="rect"><br />
+
+        <label for="tri">Triangle</label>
+        <input type="radio" id="tri" name="shape" value="tri"><br />
+
+        <label>Enter height:</label> <input type="number" name="ht" value="" ><br />
+
+        <label>Enter width: </label> <input type="number" name="wh" value=""><br />
+
+        <label><input type="submit" value="Submit" name="submit"></label><br />
+        </form>
+
+
+<?php
+        class Shape {
+            //Properties
+            private $height;
+            private $width;
+           
+            
+            //Methods
+            function __construct ($height, $width) {
+            
+                $this->height = $height;
+                $this->width = $width;
+        
+
+            }
+        
+            
+            function get_height (){
+                return $this->height;
+            }
+            
+            function get_width (){
+                return $this->width;
+            }
+
+        
+        }
+        
+        class Rectangle extends Shape {
+
+
+            public function calc_area ($sh) {
+        
+                $area = $sh->get_height() * $sh->get_width();
+                return $area;
+
+            }
+
+        }
+
+
+        class Triangle extends Shape {
+
+
+            public function calc_area ($sh) {
+        
+                $area =  $sh->get_height() * $sh->get_width() / 2;
+                return $area;
+               
+
+            }
+            
+        }
+
+        
+            if (isset($_POST['submit'])) {
+                $height = $_POST['ht'];
+                $width = $_POST['wh'];
+                $shape = $_POST['shape'];
+            
+
+                if ($shape == "rect") {
+                $sh = new Rectangle ($height, $width);
+                } 
+                else {
+                $sh = new Triangle ($height, $width);
+                }
+            
+              echo "The area of " . get_class ($sh) . " is: " . $sh->calc_area($sh);
+             
+            
+            
+                }
+            
+        
+            
+        
+            ?>
+
+
+    </body>
+    </html>
