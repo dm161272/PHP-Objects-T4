d5f9cfac19470b37322a51b311ddf878f2942016
dm161272 <58472537+dm161272@users.noreply.github.com>
2022-05-11 12:20:48 +0200

    Update ex1.php

--- n1ex1t4PHP/ex1.php
+++ n1ex1t4PHP/ex1.php
@@ -66,7 +66,7 @@ the name and a message if he has or has not paid taxes (if the salary exceeds 60
             private static $taxbias = 6000;
             
             //Methods
-            function __construct ($first_name, $last_name, $salary) {
+            public function __construct ($first_name, $last_name, $salary) {
             
             $this->first_name = $first_name;
             $this->last_name = $last_name;
@@ -75,14 +75,14 @@ the name and a message if he has or has not paid taxes (if the salary exceeds 60
         
             
             //function to print employee data
-            function to_print(){
+            public static function to_print($employee){
             $taxable = "is taxable.";
             
-            if ($this->salary <= Employee::$taxbias) {
+            if ($employee->salary <= Employee::$taxbias) {
                 $taxable = "is NOT taxable.";
             }
             
-            echo "<b>Employee: </b>" . $this->first_name . " " . $this->last_name . ", salary of " . $this->salary . " EUR " . $taxable . "<br />";
+            echo "<b>Employee: </b>" . $employee->first_name . " " . $employee->last_name . ", salary of " . $employee->salary . " EUR " . $taxable . "<br />";
             
             }
 
@@ -96,7 +96,7 @@ the name and a message if he has or has not paid taxes (if the salary exceeds 60
             
                 $employee = new Employee ($first_name, $last_name, $salary);
               
-                $employee->to_print();
+                Employee::to_print($employee);
             
             
                 }
