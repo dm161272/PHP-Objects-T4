264930d33021222b820d69954f922fc4e33a370b
dm161272 <58472537+dm161272@users.noreply.github.com>
2022-05-12 12:40:23 +0200

    Update ex1.php

--- n2ex1t4PHP/ex1.php
+++ n2ex1t4PHP/ex1.php
@@ -14,39 +14,42 @@ Make an app that allows you to roll five poker dice at once.
 
 Also, program the *getTotalThrows method which has to show the total number of throws among all the dice
 
+***********************************
+In Level 2, why are you merging the throw and the show steps? Also, you can improve your program thinking about how to implement the faces of the dice ;)
+
+Other thing its, why don't you create 5 dice and then throw it the times that you want? Its better that creating a dice eeevery time.
 
 */
+
 echo "<title>*POKER*</title>";
 
 
     
 class PokerDice {
 
-private $dice_array = array ();
+private static $dice_array = array ("Ace", "K", "Q", "J", "7", "8");
 private static $counter = 0;
 
 
 
 function __construct () {
 
-$this->dice_array = array ("Ace", "K", "Q", "J", "7", "8");
+$dice_array;
 
 }
 
 
 function throw () {
 
-  $th = rand (0, 5);
+  $index = rand (0, 5);
   self::$counter++;
-  return $th;
+  return $index;
 
 }
 
-function side_up ($dice) {
+function side_up ($index) {
 
-  $index = $dice->throw();
- 
-  $up = $dice->dice_array[$index];
+  $up = PokerDice::$dice_array[$index];
   return $up;
 }
 
@@ -60,26 +63,28 @@ function get_total_throws () {
 }
 
 
-
 echo "<h1>Poker game</h1>";
-for ($j = 0; $j<=9; $j++) {
-
+//creating 5 dice cubes
 for ($i = 0; $i <= 4; $i++) {
 
-$dice = "dice" . ($i + 1);
+    $dice = "dice" . ($i + 1);
+    
+    $$dice = new PokerDice;;
+}
 
-$$dice = new PokerDice;;
+//making 10 throws of created dices
+for ($j = 0; $j <= 9; $j++) {
 
-//echo var_dump ($$dice);
+    $throw = $$dice->throw();
 
-$turn = $$dice->side_up($$dice);
+    $turn = $$dice->side_up($throw);
 
 echo $turn . " ";
 
-}
 echo "<br \>";
 }
-echo "<h2>Number of throws: " . $$dice->get_total_throws() . "</$turn>";
 
+
+echo "<h2>Number of throws: " . $$dice->get_total_throws() . "</$turn>";
 
 
